3a99302b3304af79dcc596e31be5b8e87e8483fa
WebertySilveira <weberty.silveira.sousa@gmail.com>
2021-09-08 22:53:23 -0300

    Realizar Ajustes

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import requests
+import json
 
+process = input()
+process = process.replace('.', '')
+process = process.replace('-', '')
+
+url = "https://python.doc9.com.br/api/teste_programador/?processo=" + process
 header = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'}
-request = requests.get("https://python.doc9.com.br/api/teste_programador/?processo=00003825420205170014", headers = header).json()
+request = requests.get(url, headers = header).json()
